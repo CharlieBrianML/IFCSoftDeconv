7dfb5b347a01e63f20b74194429cf250291bb7c7
CharlieBrianML <charls_brian@hotmail.es>
2020-02-25 13:40:02 -0600

    matrix.py: Se ha creado la imagen MyImage.png

--- matrix.py
+++ matrix.py
@@ -7,11 +7,31 @@ data = np.fromfile(binaryFile, dtype='>d') # reads the whole file
 
 cont=0
 turn=False
-aux = np.arange(0,10)
+#aux = np.arange(0,10)
 matrix = np.arange(0,40)
 
+#Transformacion del arreglo unidimensional a bidimencional
+def transformar(data):
+    contRow=0
+    matrixB = np.empty((1, 255))
+    #aux = np.array([[None, None, None, None, None, None, None, None, None, None]])
+    aux = np.empty((1, 255))
+    #matrixB = np.arange(0,10)
+    for l in range(65025):
+            if (contRow==254):
+                aux[0][contRow]=data[l]*10e100
+                #matrixB = numpy.array([[1, 2, 3], [4, 5, 6]])
+                matrixB = np.append(matrixB, aux, axis = 0)
+                contRow=0
+            else:
+                aux[0][contRow]=data[l]*10e100
+                contRow=contRow+1
+    print(matrixB)
+    print(aux)
+    return matrixB
+
 #Codigo para graficar los valores
-def graficar():
+def graficar(data):
     x = np.arange(0,65536)#Generamos los valores de x
     def f(x):
         return 10000000e300*np.take(data,x)#Obtenemos la correspondencia del vector
@@ -35,8 +55,8 @@ def girar():
                             matrix[i]=aux[-(cont+1)]
                     cont=cont+1
 
-def crearImagen():
-    cv2.imwrite("MyImage.png",data)
+def crearImagen(matrixB):
+    cv2.imwrite("MyImage.png",matrixB)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
@@ -58,6 +78,9 @@ def recortar():
     #matrix=np.delete(matrix,0)
     print(indices)
 
+#transformar(data)
+#graficar(data)
+crearImagen(transformar(data))
 print (matrix)
 binaryFile.close()
 
