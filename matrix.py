2af9b6e3c3245a6ec29465083d34afbb92fb6c3a
CharlieBrianML <charls_brian@hotmail.es>
2020-02-26 17:29:12 -0600

    matrix.py: Imagen amarilla

--- matrix.py
+++ matrix.py
@@ -3,6 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 binaryFile = open("imagen1.dat", mode='rb')#Abrimos el archivo en modo binario
+#byte = binaryFile.read()
 data = np.fromfile(binaryFile, dtype='>d') # reads the whole file
 
 cont=0
@@ -26,15 +27,19 @@ def transformar(data):
             else:
                 aux[0][contRow]=data[l]*10e100
                 contRow=contRow+1
-    print(matrixB)
-    print(aux)
+    #print(matrixB)
+    #print(aux)
     return matrixB
 
 #Codigo para graficar los valores
 def graficar(data):
+    valor=0
     x = np.arange(0,65536)#Generamos los valores de x
     def f(x):
-        return 10000000e300*np.take(data,x)#Obtenemos la correspondencia del vector
+        valor=100000000e300*np.take(data,x)#Obtenemos la correspondencia del vector
+        data[x]=valor*1000000000
+        return data[x]
+        #return 100000000e300*np.take(data,x)#Obtenemos la correspondencia del vector
     plt.plot(x,f(x))#Graficamos
     plt.show()#Mostramos en pantalla
 
@@ -56,9 +61,15 @@ def girar():
                     cont=cont+1
 
 def crearImagen(matrixB):
-    cv2.imwrite("MyImage.png",matrixB)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imwrite("MyImage.png",matrixB)
+    #img = cv2.imread("MyImage.png")
+    #gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #plt.imshow(img, cmap='gray')
+    plt.imshow(matrixB, vmin=0,vmax=10)
+    plt.show()
+    #cv2.imshow("Gray Scale Image", gray_img)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()"""
 
     
 #Codigo para recortar la imagen, quitamos elementos la matriz
@@ -78,10 +89,10 @@ def recortar():
     #matrix=np.delete(matrix,0)
     print(indices)
 
-#transformar(data)
-#graficar(data)
-crearImagen(transformar(data))
-print (matrix)
+graficar(data)
+matrixB=transformar(data)
+crearImagen(matrixB)
+print (data)
 binaryFile.close()
 
 #Para mas informacion consulta: https://github.com/CharlieBrianML/InstitutoFisiologiaCelularSS
