3586380db3d3736859db05800df7cde17abdd9b4
CharlieBrianML <charls_brian@hotmail.es>
2020-03-09 12:36:15 -0600

    Se ha agregado una funcion de fase inversa

--- prueba.py
+++ prueba.py
@@ -3,17 +3,6 @@ data = np.array([[1,2,3],[4,5,6],[7,8,9]])
 data2 = np.array([1,2,3,4,5,6,7,8,9])
 data3 = np.empty(9)
 print(data2)
-
-def girar(data):
-	turn=False
-	aux = np.empty((3))
-	for i in range(3):
-		if(turn==True):
-			for k in range(3):
-				aux[k]=data[i][k]
-			for l in range(3):
-				data[i][l]=aux[-(l+1)]
-		turn=not(turn)
 		
 def fase(numFase,data2):
     for j in range(numFase):
@@ -23,7 +12,7 @@ def fase(numFase,data2):
             else:
                 data3[i]=data2[i+1]
         data2=data3
-        print(data3)
+        print(data2)
     return data2
         
 #girar(data)
